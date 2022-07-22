f629e49eff91d1db7eca9e20fb8ede56833fc260
LS <lavanya.sethuraman23@gmail.com>
2022-07-22 19:28:58 -0400

    solved

--- src/02-dark-mode/DarkMode.js
+++ src/02-dark-mode/DarkMode.js
@@ -1,8 +1,10 @@
 export default function DarkMode () {
   return (
     <div className='page'>
-      <button className='dark-mode-button'>Dark Mode</button>
-      <button className='light-mode-button'>Light Mode</button>
+      <button className='dark-mode-button' onClick={() => document.querySelector('.page').className += " dark-mode"
+      }>Dark Mode</button>
+      <button className='light-mode-button' onClick={() => document.querySelector('.page').classList.remove("dark-mode")
+    }>Light Mode</button>
     </div>
   )
 }
