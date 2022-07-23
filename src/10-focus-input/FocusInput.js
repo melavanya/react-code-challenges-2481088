323d9dc55cf9592ef6b97052fe4359ed41f4cb69
LS <lavanya.sethuraman23@gmail.com>
2022-07-23 14:26:15 -0400

    solved

--- src/10-focus-input/FocusInput.js
+++ src/10-focus-input/FocusInput.js
@@ -1,8 +1,14 @@
+import { useEffect, useRef } from "react";
+
 export default function FocusInput() {
+  const inputEl = useRef(null);
+  useEffect(() => {
+    inputEl.current.focus();
+  },[])
   return (
     <div>
       <label htmlFor='focused-input'>Focus me on page load!</label>
-      <input name='focused-input'></input>
+      <input ref={inputEl} name='focused-input'></input>
     </div>
   )
 }
