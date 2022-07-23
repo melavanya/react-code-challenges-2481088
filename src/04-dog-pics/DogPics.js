b225a1458068d739d957065bac358be6a691aab7
LS <lavanya.sethuraman23@gmail.com>
2022-07-22 21:28:32 -0400

    solved

--- src/04-dog-pics/DogPics.js
+++ src/04-dog-pics/DogPics.js
@@ -1,9 +1,17 @@
+import React, { useEffect, useState } from "react";
+
 export default function DogPics () {
-  // API: https://dog.ceo/dog-api/
+  const API = 'https://dog.ceo/api/breeds/image/random';
+  const [dogImage, setDogImage] = useState("https://images.dog.ceo/breeds/spaniel-cocker/n02102318_4172.jpg")
+  const loadDog = async () => {
+    const data = await fetch(API);
+    const dogData = await data.json();
+    setDogImage (dogData.message);
+  }
   return (
     <div className='dog-pics'>
-      <img src='https://images.dog.ceo/breeds/spaniel-cocker/n02102318_4172.jpg' />
-      <button>🐶</button>
+      <img src={dogImage} />
+      <button onClick={loadDog}>🐶</button>
     </div>
   )
 }
